d35c426d4b139a37aa23ad5fb4543745f64f8b4e
amit joshi <amitjoshi3101@gmail.com>
2022-01-24 16:05:33 +0530

    first

---
+++ src/test/java/study/TestMain.java
@@ -0,0 +1,15 @@
+package study;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+	@Test
+	public void testInputIsEven(){
+		assertTrue(Main.checkIfInputIsAnEvenNumber(23)); 
+		//Assertion
+	}
+	
+}
